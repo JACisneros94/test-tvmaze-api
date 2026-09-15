c02b152edf5822ad6404e3dc4cdb3d6ab43c0c25
JACisneros94 <jacisnerosp.94@gmail.com>
2026-09-14 20:04:19 -0600

    proyecto base Spring Boot con configuracion MongoDB Atlas

---
+++ src/main/java/com/tvmaze/api/TvMazeApiApplication.java
@@ -0,0 +1,12 @@
+package com.tvmaze.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvMazeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvMazeApiApplication.class, args);
+    }
+}
